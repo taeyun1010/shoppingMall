55b84b43c782089636fd5ceaa9cd2ed1a06ba340
Taeyun Kim <taeyun1010@yahoo.ca>
2021-06-15 09:13:08 +0900

    Updates

---
+++ precache-manifest.8970bb2d2abdd2fc8fe82283ddcf2955.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fee13d2f95db4554556fe49a38c579e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe88808e1c67ddb13aed",
+    "url": "/static/css/2.c141373c.chunk.css"
+  },
+  {
+    "revision": "2f1ab1b4b1499c3daa9f",
+    "url": "/static/css/main.d2c8346d.chunk.css"
+  },
+  {
+    "revision": "fe88808e1c67ddb13aed",
+    "url": "/static/js/2.8346b337.chunk.js"
+  },
+  {
+    "revision": "c0554f266784fabb0402ff1139c47fda",
+    "url": "/static/js/2.8346b337.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f1ab1b4b1499c3daa9f",
+    "url": "/static/js/main.502077fe.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  }
+]);
